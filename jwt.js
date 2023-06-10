03701a0ab40fc3851b7d3e737556b0a10c43407d
phoenix9469 <phoenix9469@kakao.com>
2023-06-11 00:15:26 +0900

    인증코드 기능 구현

--- jwt.js
+++ jwt.js
@@ -13,7 +13,7 @@ require('console-stamp')(console, 'yyyy/mm/dd HH:MM:ss.l');
 const options = {
     key: fs.readFileSync('./privkey.pem'),
     cert: fs.readFileSync('./cert.pem')
-};  
+};
 
 const app = express();
 
@@ -56,31 +56,42 @@ const generateRefreshToken = (id) => {
 app.post("/sign", (req, res) => {
     let id = req.body.id;
     let pw = req.body.pw;
-
-    //DB에 중복되는 값 있는지 확인
-    connection.query(`SELECT id FROM user WHERE id = ?;`, [id], function (error, results) {
-        let type = new Array();
+    let confirm_code = req.body.confirm_code;
+    connection.query(`SELECT confirm_code FROM confirm WHERE confirm_code = ?;`, [confirm_code], function (error, confirm_results) {
         if (error) {
-            console.log('SELECT id FROM user WHERE id = ? Error');
+            console.log('SELECT confirm_code FROM confirm WHERE confirm_code = ? Error');
             console.log(error);
             return;
         }
-        //중복이면 return
-        if (results.length > 0) {
-            res.status(400).send('User ID Conflict')
+        if (confirm_results.length == 0) {
+            res.status(400).send('No matching code')
             return;
-        } else {//중복 아니면 DB에 ID,PW등록
-            connection.query(`INSERT INTO user (id, pw) VALUES (?,?);`, [id, pw], (insert_error, insert_results) => {
-                if (insert_error) {
-                    console.log('User Insert Error');
-                    console.log(insert_error);
-                    res.sendStatus(500);
-                    return;
-                }
-                console.log(insert_results);
-                res.sendStatus(200);
-            });
         }
+        //DB에 중복되는 값 있는지 확인
+        connection.query(`SELECT id FROM user WHERE id = ?;`, [id], function (error, results) {
+            let type = new Array();
+            if (error) {
+                console.log('SELECT id FROM user WHERE id = ? Error');
+                console.log(error);
+                return;
+            }
+            //중복이면 return
+            if (results.length > 0) {
+                res.status(400).send('User ID Conflict')
+                return;
+            } else {//중복 아니면 DB에 ID,PW등록
+                connection.query(`INSERT INTO user (id, pw) VALUES (?,?);`, [id, pw], (insert_error, insert_results) => {
+                    if (insert_error) {
+                        console.log('User Insert Error');
+                        console.log(insert_error);
+                        res.sendStatus(500);
+                        return;
+                    }
+                    console.log(insert_results);
+                    res.sendStatus(200);
+                });
+            }
+        });
     });
 });
 
@@ -143,7 +154,7 @@ app.post("/refresh", (req, res) => {
 
             const accessToken = generateAccessToken(user.id);
 
-            res.json({ accessToken, refreshToken});
+            res.json({ accessToken, refreshToken });
         }
     );
 });
@@ -163,7 +174,7 @@ frontend.on('connection', socket => {
     socket.on('request_data_all', request_data => {
         const { user_id } = request_data;
         //Application과 Frontend에 현재 상태 DB 넘기기
-        connection.query(`SELECT * FROM device_data WHERE user_id = ?;`, [user_id],function (error, results) {
+        connection.query(`SELECT * FROM device_data WHERE user_id = ?;`, [user_id], function (error, results) {
             if (error) {
                 console.log('SELECT * FROM device_data error');
                 console.log(error);
